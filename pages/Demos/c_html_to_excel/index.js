3101c010d08a678c8efafac4a4a4f5e3053b527b
liuyib <1656081615@qq.com>
2019-03-21 15:24:38 +0800

    修改demo样式

--- pages/Demos/c_html_to_excel/index.js
+++ pages/Demos/c_html_to_excel/index.js
@@ -1,33 +1,40 @@
 window.onload = function() {
-  var oTable = document.getElementById("table_wrapper").innerHTML;
+  var oTable = document.getElementById("table").outerHTML;
   var excelHtml = `
       <html>
       <head>
         <meta charset='utf-8' />
         <style>
-          .tableA {
+          #table_wrapper {
+            width: 600px;
+            text-align: center;
+            margin: 50px auto;
+          }
+          #table {
+            width: 600px;
+            border: 1px solid #000;
             border-collapse: collapse;
+            margin: 0 0 20px 0;
+            font-size: 22px;
           }
-          .tableA .title th{
+          #table .title th{
             height: 50px;
-            font-size: 24px;
-            font-family: '微软雅黑';
-            font-weight: 700;
+            font-family: 'Arial';
+            font-weight: 600;
           }
-          .tableA tr th {
-            border: 1px #000 solid;
+          #table tr th {
             height: 40px;
+            border: 1px solid #000;
             background: #efefef;
           }
-          .tableA tr td {
-            padding: 0 40px;
-            border: 1px #000 solid;
+          #table tr td {
             height: 40px;
+            padding: 0 40px;
+            border: 1px solid #000;
             text-align: center;
           }
-          .tableA .footer td {
-            font-size: 20px;
-            font-weight: 700;
+          #table .footer td {
+            font-weight: 600;
           }
         </style>
       </head>
